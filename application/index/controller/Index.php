db70503f9a3eee227a3f5b88742c151e52105475
zgnMark <913804005@qq.com>
2017-10-17 16:33:29 +0800

    1.0

---
+++ application/index/controller/Index.php
@@ -0,0 +1,51 @@
+<?php
+namespace app\index\controller;
+
+class Index extends \think\Controller
+{
+		public function index()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function introduction()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		public function login()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function pay()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function register()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function search()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function shopcart()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function sort()         
+		    {                           
+		    	return $this->fetch('sort');    
+		    }                           
+		
+		public function success1()         
+		    {                           
+		    	return $this->fetch('success');    
+		    }                         
+		          
+		         			  
+}                   
---
+++ application/index/controller/Person.php
@@ -0,0 +1,135 @@
+<?php
+namespace app\index\controller;
+
+class Person extends \think\Controller
+{
+		public function address()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		public function bill()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function billlist()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function bindphone()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function blog()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function bonus()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function change()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function collection()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function comment()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function commentlist()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function coupon()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function email()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function foot()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function idcard()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function index()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function information()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function logistics()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function news()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function order()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function orderinfo()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function password()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function question()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function record()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function refund()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function safety()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+		public function setpay()         
+		    {                           
+		    	return $this->fetch();    
+		    }                           
+		
+}                   
